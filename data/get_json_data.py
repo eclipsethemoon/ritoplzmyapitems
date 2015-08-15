d0bf24c5b72d8fb238d08eea2befc0e75180fae1
swon <stephen.min.won@gmail.com>
2015-08-14 23:35:51 -0400

    Created script to gather all the match data without breaking the rate limit (delay of 1.2s per request).

--- data/get_json_data.py
+++ data/get_json_data.py
@@ -1,15 +1,30 @@
 import re
+import requests
+import time
+import yaml
 
-patches = ['5.11', '5.14']
-match_types = ['NORMAL_5X5', 'RANKED_SOLO']
-regions = ['BR', 'EUNE', 'EUW', 'KR', 'LAN', 'LAS', 'NA', 'OCE', 'RU', 'TR']
+if __name__ == "__main__":
+    with open('conf/application.yml', 'r') as f:
+        config = yaml.load(f)
 
-for patch in patches:
-    for match in match_types:
-        for region in regions:
-            f_json = open('/'.join([patch, match, region]) + '_data.json', 'a')
-            with open('/'.join([patch, match, region]) + '.json', 'r') as f_id:
-                for line in f_id:
-                    match_id = re.search(r'[0-9]+', line)
-                    
+    patches = ['5.11', '5.14']
+    match_types = ['NORMAL_5X5', 'RANKED_SOLO']
+    regions = ['BR', 'EUNE', 'EUW', 'KR', 'LAN', 'LAS', 'NA', 'OCE', 'RU', 'TR']
+
+    for patch in patches:
+        for match in match_types:
+            for region in regions:
+                print('Current file: ' + '_'.join([region, match, patch]))
+                f_json = open('json/' + '_'.join([region, match, patch]) + '.json', 'a')
+                with open('/'.join([patch, match, region]) + '.json', 'r') as f_id:
+                    for line in f_id:
+                        match_id = re.search(r'[0-9]+', line)
+                        if match_id is not None:
+                            res = requests.get('https://' + region.lower() + '.api.pvp.net/api/lol/' + region.lower() +
+                                               '/v2.2/match/' + match_id.group() + '?includeTimeline=true&api_key=' +
+                                               config['dev_api_key'])
+                            if res.status_code == 200:
+                                f_json.write(res.content + "\n")
+                                time.sleep(1.2)
+
 
