4b799309ffb8d26dd0c3cf2f31effab28b17960d
issenec <stephen.min.won@gmail.com>
2015-08-26 03:47:46 -0400

    Updated the data and cleanup script.

--- data/clean_processed.sh
+++ data/clean_processed.sh
@@ -1,2 +1,2 @@
-rm -rf json/champions json/items json/processed json/production json/reduced
-mkdir json/champions json/items json/processed json/production json/reduced
+rm -rf json/champions json/items json/processed json/production json/reduced json/data
+mkdir json/champions json/items json/processed json/production json/reduced json/data
